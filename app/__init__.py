c9d36c12d1cf1ebbca67280b0651eccd0286b37f
minhhnghi123 <minhhnghiii@gmail.com>
2025-05-09 08:31:41 +0700

    init

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app():
+    # Explicitly set the template folder path
+    app = Flask(__name__, template_folder="../templates")
+    app.config['SECRET_KEY'] = 'your_secret_key'
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
