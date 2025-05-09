b632ebec94977854cf9f5d2bf2c38e647592b5e2
Alien <minhhnghiii@gmail.com>
2025-05-09 17:56:10 +0700

    fix: static, css

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     # Explicitly set the template folder path
-    app = Flask(__name__, template_folder="../templates")
+    app = Flask(__name__, template_folder="../templates",static_folder ="../static")
     app.config['SECRET_KEY'] = 'your_secret_key'
 
     from .routes import main
