539d3a8ffe8af3d53b8b0cfb7fdf52bff0673d35
minhnghi123 <minhhnghiii@gmail.com>
2025-05-11 09:57:17 +0700

    feat:ui company

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/company")
+def company():
+    return render_template("company.html")
+
+@main.route("/company/<string:company_name>")
+def company_details(company_name):
+    return render_template("company_details.html", company_name=company_name)
