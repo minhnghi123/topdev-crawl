3bf07b3ca15c4e9041b97b84c9901f6dedf7cb18
hongphuc <phucpph.10a9bb.1920@gmail.com>
2025-05-11 10:01:47 +0700

    index
    index

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,9 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+@main.route("/search")
+def search():
+    return render_template("search.html")
+@main.route("/thongke")
+def thongke():
+    return render_template("thongke.html")
