1b8319788f1c1f9dd90ba8a26637f66e3be804e4
Alien <minhhnghiii@gmail.com>
2025-05-18 09:13:24 +0700

    push json to db

--- Data/company/push_company_details.py
+++ Data/company/push_company_details.py
@@ -1,22 +1,21 @@
 import sqlite3
 import json
-
-with open('Data/company/companyDetails.json', 'r', encoding='utf-8') as f:
+from push_products import push_products_to_db
+from push_company_skill import push_company_skill_to_db
+with open('companyDetails.json', 'r', encoding='utf-8') as f:
     companies = json.load(f)
 
-conn = sqlite3.connect('Data/crawl.db')
+conn = sqlite3.connect('../crawl.db')
 cursor = conn.cursor()
 
-for company in companies:
+for company in companies: 
     name = company.get('name', '')
     logo = company.get('logo', '')
     shortdescription = company.get('slogan', '')
-    curent_job_opening = int(company.get('jobs_count', 0)) if company.get('jobs_count', '').isdigit() else 0
+    curent_job_opening = int(company.get('jobs_count', '0').strip()) if company.get('jobs_count', '0').strip().isdigit() else 0
     industry = company.get('industry', company.get('field', ''))
     size = company.get('company_size', '')
     nationality = company.get('nationality', {}).get('name', '')
-    # TODO: CHECK
-    tech_stack = ', '.join(company.get('skills', []))
     website = company.get('website', '')
     social_media = ', '.join([s.get('url', '') for s in company.get('social_links', [])])
     address = company.get('address', '')
@@ -24,12 +23,16 @@ for company in companies:
     banner = company.get('banner','') ; 
     short_address = company.get('short_address','') ; 
     followers = int(company.get('followers',0)) if company.get('followers','').isdigit() else 0 ;
-    about_images = ', '.join([s.get('url', '') for s in company.get('about_images', [])])
+    about_images = ', '.join([s for s in company.get('about_images', [])])
     cursor.execute('''
-        INSERT INTO company (Logo, name, shortdescription, curent_job_opening, industry, size, nationality, tech_stack, website, Social_media, address, description)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (logo, name, shortdescription, curent_job_opening, industry, size, nationality, tech_stack, website, social_media, address, description,banner, short_address, followers, about_images))
-
+        INSERT INTO company (Logo, name, shortdescription, curent_job_opening, industry, size, nationality, website, Social_media, address, description,banner,short_address, followers, about_images)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (logo, name, shortdescription, curent_job_opening, industry, size, nationality, website, social_media, address, description,banner, short_address, followers, about_images)) ; 
+    
+    # get id
+    company_id = cursor.lastrowid  ;
+    push_products_to_db(company,company_id,cursor) ;
+    push_company_skill_to_db(company,company_id,cursor) 
+    print('Company details pushed to database.')
 conn.commit()
 conn.close()
-print('Company details pushed to database.')
